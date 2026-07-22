0cc18532dae6b1fe7ed76e057610d9eae9c0d1d8
Aryanbhargava18 <aryanbhargava644@gmail.com>
2026-07-22 14:41:11 +0530

    feat: lower default complexity limit to 200 and add validation
    Signed-off-by: Aryanbhargava18 <aryanbhargava644@gmail.com>

--- chaoscenter/graphql/server/server.go
+++ chaoscenter/graphql/server/server.go
@@ -150,7 +150,12 @@ func main() {
 	if enableIntrospection {
 		srv.Use(extension.Introspection{})
 	}
-	srv.Use(extension.FixedComplexityLimit(utils.Config.GqlQueryComplexityLimit))
+	limit := utils.Config.GQLQueryComplexityLimit
+	if limit > 0 {
+		srv.Use(extension.FixedComplexityLimit(limit))
+	} else {
+		log.Warnf("invalid GQL query complexity limit %d; skipping complexity limiting", limit)
+	}
 
 	// GraphQL operation tracking middleware
 	srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
